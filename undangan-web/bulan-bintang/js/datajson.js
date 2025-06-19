20a9e0e939f55e5dfe0e686f8b6e0517a10a85e2
wahyuajismustofa <wahyuajismustofa333@gmail.com>
2025-06-19 07:16:38 +0700

    Auto-sync 07:16 19/06/2025

--- undangan-web/bulan-bintang/js/datajson.js
+++ undangan-web/bulan-bintang/js/datajson.js
@@ -5,7 +5,7 @@ function getParam(variabel) {
 }
 // variabel global
 const idTamu = getParam("id");
-const SCRIPT_BASE_URL = "https://script.google.com/macros/s/AKfycbzdeDedxGQdwzjw52mLupTxqY8bT4AcnOFBvbW4qbFU3EQ6Jpxy68JZbTHPHDlDlCSJ/exec";
+const SCRIPT_BASE_URL = "https://script.google.com/macros/s/AKfycbxLKt-nSIyhevMLW5FFtvYh_avKS2LNNoE4HiRgPzpEDY5pfuOmwm6Cjb_emMmfJRel/exec";
 const DATABASE_NAME = "test-undangan-web";
 let data_update;
 
@@ -260,7 +260,7 @@ async function handleFormGift(event) {
   event.preventDefault();
   const form = document.getElementById("weddingGiftForm");
   const akun = form.querySelector('[name="nama-akun"]').value.trim();
-  const pesan = form.querySelector('[name="pesan"]').value.trim().replace(/ /g, "_");
+  const pesan = form.querySelector('[name="pesan"]').value.trim().replace(/ /g, "_").replace(/,/g, "{koma}");
   const nominal = form.querySelector('[name="nominal"]').value.trim();
   if (!akun || !pesan || !nominal) return alert("Silakan lengkapi data kado");
 
@@ -322,7 +322,7 @@ function getComment() {
 
   return komentar.value
     .trim()
-    .replace(/\r?\n/g, "\\n")
+    .replace(/,/g, "{koma}")
     .replace(/ /g, "_");
 }
 
